fd7f64e7b862a9636732648713c7b9bb85f242e0
eirenefc <eirene.fireman@fcamara.com.br>
2021-12-15 20:53:20 -0300

    criando classe principal

---
+++ Main.java
@@ -0,0 +1,11 @@
+import analisadores.lexico.Lexico;
+import analisadores.lexico.Tokens;
+import analisadores.lexico.TokensClass;
+
+public class Main {
+    public static void main(String[] args) {
+		Lexico lexical = new Lexico("./programs/test.bfs");
+		TokensClass tokens = null;
+	}
+
+}
